6ec05361c3ae3aeffeed55b4d508447e2c3a8076
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-10-31 19:55:06 -0400

    Update query to proctect from sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,18 +9,19 @@ const pool = new Pool({
 
 let userInput = process.argv
 
-console.log(userInput);
-
 let cohortName = userInput[2];
 let limit = userInput[3];
+let values = [`%${cohortName}%`, limit];
+let queryString = `
+  SELECT students.id, students.name, cohorts.name as cohort_name
+  FROM students
+  JOIN cohorts ON cohorts.id = students.cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-pool.query(`
-SELECT students.id, students.name, cohorts.name as cohort_name
-FROM students
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${cohortName}%'
-LIMIT ${limit};
-`)
+
+pool.query(queryString,values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`)
